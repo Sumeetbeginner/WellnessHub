0a5eea48697c2c2ec2a8f281bc8e9cc15c9552e3
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-01-17 21:54:02 +0530

    dark & light mode

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+
+import { changeTheme } from ".";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJwPz8g3mSrtoOllv5t1XUjfCOtOfEFkE",
+    authDomain: "cogno-solution.firebaseapp.com",
+    databaseURL: "https://cogno-solution-default-rtdb.firebaseio.com",
+    projectId: "cogno-solution",
+    storageBucket: "cogno-solution.appspot.com",
+    messagingSenderId: "132297251528",
+    appId: "1:132297251528:web:6b4d11b1ffc5db790fe3fd",
+    measurementId: "G-GE4E9MSXRH"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+changeTheme()
+
+
